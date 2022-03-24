0a66871a938cbbb619b7f5ddbb65de109c0a71e9
1pei0 <91524786+1pei0@users.noreply.github.com>
2022-03-24 10:11:50 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaProvider} from 'react-native-safe-area-context';
+import { NativeBaseProvider } from 'native-base';
+import Navigation from './src/navigation';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider bg="#fff"  h="0">
+      {/* <SafeAreaView style={{ flex: 1 }}> */}
+        <NativeBaseProvider bg="#fff" >
+        <Navigation />
+        </NativeBaseProvider>
+      {/* </SafeAreaView> */}
+     </SafeAreaProvider>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
